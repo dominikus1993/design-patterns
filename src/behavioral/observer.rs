0161f1fbfa5a75a43119068667ede3d378257b6b
Dominik.Kotecki <dominikus1910@gmail.com>
2024-08-16 08:23:33 +0000

    add unsubscibe pattern

--- src/behavioral/observer.rs
+++ src/behavioral/observer.rs
@@ -1,10 +1,19 @@
 trait Observer<T> {
+    fn get_id(&self) -> i32;
     fn on_update(&mut self, value: T);
 }
 
 #[derive(Debug, Clone, Copy)]
 struct IntObserver {
+    id: i32,
     current_value: i32,
+}
+
+impl IntObserver {
+    fn new(id: i32) -> IntObserver {
+        IntObserver{id: id, current_value: 0}
+    }
+    
 }
 
 impl Observer<i32> for IntObserver {
@@ -12,10 +21,15 @@ impl Observer<i32> for IntObserver {
         self.current_value = value;
         print!("Value: {}", value);
     }
+    
+    fn get_id(&self) -> i32 {
+        return self.id;
+    }
 }
 
 trait Subject<'a, T> {
     fn subscribe(&mut self, subject: &'a mut dyn Observer<i32>);
+    fn unsubscribe(&mut self, id: i32);
     fn notify_observers(&mut self);
 }
 
@@ -33,6 +47,10 @@ impl<'a> Subject<'a, i32> for IntSubject<'a> {
         for observer in self.observers.iter_mut() {
             observer.on_update(self.value);
         }
+    }
+    
+    fn unsubscribe(&mut self, id: i32) {
+        self.observers.retain(|x| x.get_id() != id);
     }
 }
 
@@ -58,7 +76,7 @@ mod tests {
     #[test]
     fn test_observable() {
         let mut subject = IntSubject::new(0);
-        let mut observer = IntObserver{current_value: 0};
+        let mut observer = IntObserver::new(1);
 
         subject.subscribe(&mut observer);
 
@@ -70,8 +88,8 @@ mod tests {
     #[test]
     fn test_multi_observable() {
         let mut subject = IntSubject::new(0);
-        let mut observer = IntObserver{current_value: 0};
-        let mut observer2 = IntObserver{current_value: 0};
+        let mut observer = IntObserver::new(1);
+        let mut observer2 = IntObserver::new(2);
 
         subject.subscribe(&mut observer);
         subject.subscribe(&mut observer2);
@@ -80,6 +98,24 @@ mod tests {
         subject.increment();
 
         assert_eq!(observer.current_value, 2);
+        assert_eq!(observer2.current_value, 2);
+    }
+
+    #[test]
+    fn test_multi_observable_unsubscribe() {
+        let mut subject = IntSubject::new(0);
+        let mut observer = IntObserver::new(1);
+        let mut observer2 = IntObserver::new(2);
+        let id = observer.get_id();
+        subject.subscribe(&mut observer);
+        subject.subscribe(&mut observer2);
+
+        subject.unsubscribe(id);
+
+        subject.increment();
+        subject.increment();
+
+        assert_eq!(observer.current_value, 0);
         assert_eq!(observer2.current_value, 2);
     }
     
